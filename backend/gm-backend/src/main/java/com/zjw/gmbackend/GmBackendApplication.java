dc330ff286ec471908ed1e5d74077d944f66dcf7
MeraDicycle <1376541252@qq.com>
2025-12-24 09:46:28 +0800

    创建springboot3项目

---
+++ backend/gm-backend/src/main/java/com/zjw/gmbackend/GmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zjw.gmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmBackendApplication.class, args);
+    }
+
+}
---
+++ backend/gm-backend/src/test/java/com/zjw/gmbackend/GmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjw.gmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
